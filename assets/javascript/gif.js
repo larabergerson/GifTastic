569af20d1b4d0b8303c014fe573361ca28496699
lara bergerson <lara.bergerson@gmail.com>
2019-06-13 22:08:27 -0500

    linked images

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -14,46 +14,40 @@ var disneyMovies = [
 // loop to create buttons from array
 for (var i = 0; i < disneyMovies.length; i++) {
   var button = $("<button>").text(disneyMovies[i]);
-  $("#moviebtn").append(button);
-
   button.addClass("disney-button");
   button.attr("data-name", disneyMovies[i]);
-
+  $("#moviebtn").append(button);
   //   $("#moviebtn").on("click", function() {
   // function to make buttons pull data from api:
   // var moviebtn = $(this).attr("data-movies" + disneyMovies[i]);
   // $("#main").on("click", function() {
 }
-function displayGif() {
+
+function displayGif() {}
+$(document).on("click", ".disney-button", function() {
+  //we define chosenMovie based on the data-name attribute of the button we click!
+  var chosenMovie = $(this).attr("data-name"); //when using attr, if there is only 1 argument aka parameter it can be used to grab the information.
   console.log("is working");
+
   var queryURL =
     "https://api.giphy.com/v1/gifs/search?api_key=vFqWSwHKnqHyZz0kizoJcGbRSAnbHLSK&q=" +
-    disneyMovies +
+    chosenMovie +
     "&limit=10&offset=0&rating=PG&lang=en";
+
+  console.log(queryURL);
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     // console.log(response);
+    // console.log(response.data[0]);
+    console.log(response.data[0].images.fixed_height.url);
 
-    var gifUrl = response.data;
+    var gifUrl = response.data[0].images.fixed_height.url;
     var disImage = $("<img>");
-    disImage.attr("src");
+    disImage.attr("src", gifUrl);
+
+    $(".gifspot").append(disImage);
   });
-}
-// });
-
-// api query link to generate gifs.
-// var queryURL =
-//   "https://api.giphy.com/v1/gifs/search?api_key=vFqWSwHKnqHyZz0kizoJcGbRSAnbHLSK&q=disney&limit=10&offset=0&rating=G&lang=en";
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(response) {
-//   console.log(response);
-// });
-
-// var imageUrl = response.
-
-$(document).on("click", ".disney-button", displayGif);
+});
